35d389c2d750de2d98089d0af02fc9e47e5daf6a
David Thorpe <davzie@davzie.com>
2012-07-09 14:29:14 +0100

    Fixed duplicate upload models
    Signed-off-by: David Thorpe <davzie@davzie.com>

--- application/models/uploads.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Upload extends Eloquent {
-     public static $timestamps = true;
-
-     public function user()
-     {
-          return $this->belongs_to('User','user_id');
-     }
-}
